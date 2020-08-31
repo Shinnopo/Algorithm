7cae58f57863e0077d9de656b40b400a2dfa9e5a
Shinnosuke Mogi <sin0225skyhigh@icloud.com>
2020-08-31 18:21:34 +0900

    delete test

--- LinearSearch.py
+++ LinearSearch.py
@@ -3,7 +3,3 @@ def search(data, target):
         if data[i] == target:
             return i
     return -1
-
-data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-target = 7
-print("要素番号{}にデータ{}を見つけました。".format(search(data, target), target))
