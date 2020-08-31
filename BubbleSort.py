401e1ad6e8fc26b9f842bb430aea186a9e29b281
Shinnosuke Mogi <sin0225skyhigh@icloud.com>
2020-08-31 19:01:37 +0900

    delete test

--- BubbleSort.py
+++ BubbleSort.py
@@ -3,7 +3,3 @@ def sort(data):
         for j in range(i):
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
-
-data = [1, 3, 2, 5, 4, 2, 1]
-sort(data)
-print(data)
